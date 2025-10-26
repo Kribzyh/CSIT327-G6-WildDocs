0dafbfcb8fe0a26c69df71a3bc7815f51c3f041a
Kerby <delposo20@gmail.com>
2025-10-26 10:32:41 +0800

    implemented student account forget password

--- WildDocs/settings.py
+++ WildDocs/settings.py
@@ -138,3 +138,10 @@ MEDIA_ROOT = BASE_DIR / 'media'
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/dashboard/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'delposo20@gmail.com'
+EMAIL_HOST_PASSWORD = 'makm dmej acxd kipp'
