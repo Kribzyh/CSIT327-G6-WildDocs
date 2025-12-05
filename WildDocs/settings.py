ced4f2814e7c89982e77036f76d478a2d4f9ef4a
milayyyyy <camilarosecordero28@gmail.com>
2025-12-06 00:51:52 +0800

    Fix: Add email logging and use env vars for email config

--- WildDocs/settings.py
+++ WildDocs/settings.py
@@ -166,10 +166,32 @@ LOGOUT_REDIRECT_URL = '/accounts/login/'
 
 # Email configuration for forgot password reset
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'camsyyycordero@gmail.com'
-EMAIL_HOST_PASSWORD = 'tgmk wjru fana gejk'  
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', '587'))
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True').lower() == 'true'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'camsyyycordero@gmail.com')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', 'tgmk wjru fana gejk')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER)
 EMAIL_TIMEOUT = 30
+SERVER_EMAIL = DEFAULT_FROM_EMAIL
+
+# Logging for email debugging in production
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.core.mail': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+        'django': {
+            'handlers': ['console'],
+            'level': 'WARNING',
+        },
+    },
+}
