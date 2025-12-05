2b9f58342a6845d19ca5dfd22ff21c23f71972d7
milayyyyy <camilarosecordero28@gmail.com>
2025-12-06 01:44:17 +0800

    Trigger rebuild for anymail package

--- WildDocs/settings.py
+++ WildDocs/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sessions',
+    'anymail',
     "accounts",
     "index",
     "dashboard",
